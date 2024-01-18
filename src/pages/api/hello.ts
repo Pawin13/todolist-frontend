e680f13436e66fcfb06835504c027ce36edbc911
Pawinee.Ng <pawi.niyomthong@hotmail.com>
2024-01-18 22:03:35 +0700

    remove comment

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -11,27 +11,3 @@ export default function handler(
 ) {
   res.status(200).json({ name: "John Doe" });
 }
-
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import * as database from "@/lib/api/index";
-
-// type Data = {
-//   name: string;
-// };
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
-//   const route = req.query?.route as string;
-//   console.log("----->  route    ", req.query);
-//   switch (route) {
-//     case "loadUsers":
-//       const allUsers: any = await database.loadUsers(req, res);
-//       res.status(200).json(allUsers);
-//       break;
-//     default:
-//       res.status(404).end(`Not found Users Data!`);
-//       break;
-//   }
-// }
